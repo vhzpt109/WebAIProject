44e3cdd9f866c0495f6d9ca92a7c38c31b68fac6
vhzpt109 <80303325+vhzpt109@users.noreply.github.com>
2022-09-23 02:12:08 +0900

    Backbone Model Inference
    Not Working because image input format..

--- model_save.py
+++ model_save.py
@@ -1,9 +1,11 @@
 import tensorflowjs as tfjs
 from tensorflow.keras.applications import EfficientNetB0
+from tensorflow.keras.applications import MobileNetV2
 
 if __name__ == "__main__":
     model = EfficientNetB0()
+    # model = MobileNetV2()
     model.summary()
-    model.save("EfficientNetB0.h5")
 
-    tfjs.converters.save_keras_model(model, "EfficientNetB0.js")
+    tfjs.converters.save_keras_model(model, "EfficientNetB0")
+    # tfjs.converters.save_keras_model(model, "MobileNetV2")
