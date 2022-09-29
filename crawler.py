4137a0e516b26964cd8ddbbffa7b6f836a0a46b6
vhzpt109 <80303325+vhzpt109@users.noreply.github.com>
2022-09-29 23:01:44 +0900

    Crawling List Import from xlsx(not working in korean..)

--- crawler.py
+++ crawler.py
@@ -3,18 +3,24 @@ import requests
 import datetime
 from bs4 import BeautifulSoup
 import csv
+import pandas as pd
+import numpy as np
 
 from google_images_download import google_images_download
 
 
 if __name__ == "__main__":
+    df_talent_list = pd.read_excel('./talent_list.xlsx', engine='openpyxl')
+    talent_list = list(df_talent_list["name"])
+    print(','.join(talent_list))
+
     response = google_images_download.googleimagesdownload()
 
     # keywords
     # limit : number of images
     # format
     arguments = {
-        "keywords": "Cat",
+        "keywords": "고양이",
         "limit": 1000,
         "print_urls": True,
         "chromedriver": "./libs/chromedriver",
