da760fb28e0c069fcecb95ea379dd59f662271f5
vhzpt109 <80303325+vhzpt109@users.noreply.github.com>
2022-09-30 21:34:21 +0900

    google_download_images Korean Test
    Suddenly, it doesn't even work in English..

--- crawler.py
+++ crawler.py
@@ -1,18 +1,13 @@
-import time as t
-import requests
-import datetime
-from bs4 import BeautifulSoup
-import csv
+# -*- coding: utf-8 -*- 
+
 import pandas as pd
-import numpy as np
 
 from google_images_download import google_images_download
 
-
 if __name__ == "__main__":
-    df_talent_list = pd.read_excel('./talent_list.xlsx', engine='openpyxl')
-    talent_list = list(df_talent_list["name"])
-    print(','.join(talent_list))
+    # df_talent_list = pd.read_excel('./talent_list.xlsx', engine='openpyxl')
+    # talent_list = list(df_talent_list["name"])
+    # print(','.join(talent_list))
 
     response = google_images_download.googleimagesdownload()
 
@@ -21,11 +16,11 @@ if __name__ == "__main__":
     # format
     arguments = {
         "keywords": "고양이",
-        "limit": 1000,
+        "limit": 10,
         "print_urls": True,
         "chromedriver": "./libs/chromedriver",
         "format": "jpg"
-        }
+    }
 
     # passing the arguments to the function
     paths = response.download(arguments)
